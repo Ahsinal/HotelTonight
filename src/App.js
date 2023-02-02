8934dae684a2c88716150a4f99b221dd4bd46cb0
Lan <lanishakoirala23@gmail.com>
2023-02-02 21:10:02 +0545

    Initialized Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Navbar } from './Components/navBar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Hey lets make hotel Website</h1>
+    <h6>Lets Rect</h6>
+    <Navbar/>
+    </>
   );
 }
 
---
+++ src/Components/navBar/Navbar.jsx
@@ -0,0 +1,8 @@
+import './Navbar.css'
+export const Navbar=()=>{
+    return(
+        <>
+        <h1>Navbar</h1>
+        </>
+    )
+}
